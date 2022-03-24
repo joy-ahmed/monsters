f831af21fcdebfed0e65a522ac3ea1f959d394e9
joy-ahmed <esthersboydd7806@gmail.com>
2022-03-24 08:06:09 +0600

    created indivisual components for ui elements and added style to it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component { 
   constructor() {
@@ -18,10 +19,7 @@ class App extends Component {
   render() { 
     return (
       <div className="App">
-        { 
-          this.state.monsters.map
-            (list => <h1 key={list.id}>{ list.name }</h1>) 
-        }
+      <CardList monsters={ this.state.monsters }/>
       </div>
     );
   }
