59986f5f974dd313bda2f65323ac358a7dbb301c
joy-ahmed <esthersboydd7806@gmail.com>
2022-03-24 06:43:07 +0600

    started to build components of monsters card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    }
+  };
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({ monsters: users }))
+    .catch(err => console.error('Somethin went wrong :(', err))
+  }
+
+  render() { 
+    return (
+      <div className="App">
+        { 
+          this.state.monsters.map
+            (list => <h1 key={list.id}>{ list.name }</h1>) 
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
